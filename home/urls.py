85c05bb3665f6033cc733f79ab8e7a8036217761
Sergey Nikitin <sanikitin@mail.ru>
2015-11-26 10:02:24 +0300

    bootstrap changes

--- home/urls.py
+++ home/urls.py
@@ -3,5 +3,7 @@ from home import views
 
 urlpatterns = patterns('',
     url(r'^$', views.index, name='index'),
+#    url(r'^$', views.base, name='base'),
+
 #    url(r'^about/$', views.index, name='about'),
 )
--- home/views.py
+++ home/views.py
@@ -1,5 +1,17 @@
+from django.http.response import HttpResponse
+from django.template.loader import get_template
 from django.shortcuts import render
+from django.template import Context
 
 # Create your views here.
+#def index(request):
+#    return render(request, 'index.html')
+
 def index(request):
-    return render(request, 'index.html')
+    view = 'index'
+    t = get_template('home/index.html')
+    html = t.render(Context({'name': view}))
+    return HttpResponse(html)
+
+#def base(request):
+#    return render(request, 'base.html')
