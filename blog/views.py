a3422310c606a8731e09061ad4efadeab79638cc
Sergey Nikitin <sanikitin@mail.ru>
2015-11-26 15:49:03 +0300

    bootstrap changes

--- blog/views.py
+++ blog/views.py
@@ -7,3 +7,18 @@ def about(request):
 def blog(request):
     return render(request, 'blog/blog.html')
 
+# from django.http.response import HttpResponse
+# from django.template.loader import get_template
+# from django.template import Context
+#
+# def about(request):
+#     view = 'index'
+#     t = get_template('blog/about.html')
+#     html = t.render(Context({'name': view}))
+#     return HttpResponse(html)
+#
+# def blog(request):
+#     view = 'index'
+#     t = get_template('blog/blog.html')
+#     html = t.render(Context({'name': view}))
+#     return HttpResponse(html)
