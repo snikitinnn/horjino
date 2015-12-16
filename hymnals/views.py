f277b66db6844e85694207d4677ca84a2f60dc22
Sergey Nikitin <sanikitin@mail.ru>
2015-12-16 12:56:13 +0300

    errors with ws and song details were fixed. now shoulbe fixed bugs relative with page_number of song

--- hymnals/views.py
+++ hymnals/views.py
@@ -4,12 +4,11 @@ from hymnals.models import Chorus, Song, Hymnal, WS, SongvsWS
 
 def choir(request, chorus_id):
     hymnal_list = Hymnal.objects.extra(where=['chorus_id=%s'], params=[chorus_id])
-#    hymnal_list = Hymnal.objects.all()
-#    hymnal_list = hymnal_list.filter(chorus_id=chorus_id).order_by('Hymnal_Name')
     chorus = get_object_or_404(Chorus, pk=chorus_id)
     context = {'hymnal_list': hymnal_list, 'chorus':chorus}
     return render(request, 'hymnals/choir.html', context)
 
+# hymnals
 def detail(request, hymnal_id):
     selected_song_list = Song.objects.extra(where=['hymnal_id=%s'], params=[hymnal_id])
     hymnal = get_object_or_404(Hymnal, pk=hymnal_id)
@@ -27,6 +26,11 @@ def results(request, hymnal_id, song_id):
     context = {'song': song, 'hymnal': hymnal, 'song_count': song_count, 'svsws': svsws, 'date_list': date_list}
     return render(request, 'hymnals/results.html', context)
 
+def results_song(request, song_id):
+    song = get_object_or_404(Song, pk=song_id)
+    context = {'song': song, 'hymnal': song.hymnal}
+    return render(request, 'hymnals/results_song.html', context)
+
 def alphabet(request):
     alphabet_song_list = Song.objects.all().order_by('Name')
     context = {'alphabet_song_list' : alphabet_song_list}
@@ -40,32 +44,24 @@ def ws(request):
     return render(request, 'hymnals/ws.html', context)
 
 def detail_ws(request, ws_id):
-    selected_songvsws = SongvsWS.objects.extra(where=['ws_id=%s'], params=[ws_id])
     ws = get_object_or_404(WS, pk=ws_id)
-    context = {'selected_songvsws': selected_songvsws, 'ws': ws}
+    songvsws_list = SongvsWS.objects.filter(ws_id=ws_id)
+    context = {'ws':ws, 'songvsws_list':songvsws_list}
     return render(request, 'hymnals/detail_ws.html', context)
-# #    selected_songvsws = SongvsWS.objects.filter(ws_id=53)
-#     selected_songvsws_list = selected_songvsws.extra(select={'hymnal_id': 'SELECT hymnal_id FROM hymnals_hymnal,hymnals_song WHERE hymnals_song.hymnal_id = hymnals_hymnal.id'
-# #    selected_songvsws_list = selected_songvsws.extra(select={'hymnal_id': 'SELECT hymnal_id FROM hymnals_hymnal,hymnals_song, hymnals_songvsws WHERE hymnals_song.hymnal_id = hymnals_hymnal.id AND hymnals_song.id = hymnals_songvsws.song_id'})
-# #    selected_songvsws_list = selected_songvsws.select_related('song__hymnal_Hymnal_Name')
-
-
-# def detail_ws(request, ws_id):
-# #    selected_songvsws = SongvsWS.objects.extra(where=['ws_id=%s'], params=[ws_id])
-#     selected_songvsws = SongvsWS.objects.filter(ws_id=ws_id)
-#     sws = selected_songvsws.values('song_id')
-#     s = sws.select_related('song__hymnal_id')
-#     h = s.hymnal
-#
-# #    selected_songvsws_hym = selected_songvsws.extra(select={'hymnal_id': 'SELECT hymnal_id FROM hymnals_hymnal,hymnals_song WHERE hymnals_song.hymnal_id = hymnals_hymnal.id'})
-# #    selected_songvsws_list = selected_songvsws_hym.select_related('song__hymnal__Hymnal_Name')
-#     ws = get_object_or_404(WS, pk=ws_id)
-#     context = {'s': sws, 'h': h, 'ws': ws}
-# #    context = {'selected_songvsws_list' : selected_songvsws_list, 'ws': ws}
-#     return render(request, 'hymnals/detail_ws.html', context)
 
 def results_ws(request, ws_id, song_id):
     song = get_object_or_404(Song, pk=song_id)
     ws = get_object_or_404(WS, pk=ws_id)
     context = {'song': song, 'ws': ws}
     return render(request, 'hymnals/results_ws.html', context)
+
+# <!--    <TR>
+#         {% if date_list %}
+#             <h2>{{ song_count }}</h2>
+#             {% for song_vs_ws in date_list %}
+#                 <tr>
+#                     <TD>{{ song_vs_ws.Date}}</TD><TD></TD>
+#                 </tr>
+#             {% endfor %}
+#         {% endif %}
+#     </TR>-->
